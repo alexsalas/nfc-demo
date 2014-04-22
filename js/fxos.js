578fdf63d8e8b01a8bf26e1f1b68fa5121c9b86d
Eric Lee <erhlee.bird@gmail.com>
2014-04-22 17:25:26 -0400

    Update

--- js/fxos.js
+++ js/fxos.js
@@ -1,5 +1,6 @@
 // Methods that the front-end team needs
 
+// Page 5, Figure 4 (1st one)
 // Relevant API: WebNfc
 // A way to toggle NFC into 'active' mode upon tapping 'NFC + Bluetooth'
 // We can just set the appropriate handler when we need it
@@ -9,6 +10,7 @@ function nfc_active() {
 	}
 }
 
+// Page 5, Figure 4 (2nd one)
 // Relevant API: DeviceStorage
 // We need to glob the files into an email package
 // XXX: Figuring out how to do this while preserving cryptographic signing, etc
@@ -16,35 +18,61 @@ function nfc_active() {
 function nfc_email_package() {
 }
 
+// Page 6, Figure 5, (1)
 // Relevant API: Bluetooth
 // XXX: NFC hides the tech-discovery notification on the topmost WebNFC
 //		so this only lists previously paired bluetooth devices
 function paired_device_list() {
 }
 
+// Page 6, Figure 5, (2)
 // Relevant API: Bluetooth
 // XXX: Again this isn't relevant with NFC so if we do something like this, it
 //		would be through bluetooth
 function nearby_device_list() {
 }
 
+// Page 6, Figure 5, (3)
 function device_search() {
 }
 
+// Page 6, Figure 6 (1st)
 function transfer_heartbeat() {
 }
 
+// Page 6, Figure 6 (2nd)
 function transfer_complete() {
 }
 
+// Page 7, Figure 2
 function transfer_waiting() {
 }
 
+// Page 9, Figure 2
 function transfer_interrupt() {
 }
 
+// Page 9, Figures 3 and 4
 function transfer_cancelled() {
 }
 
+// Page 11, Figure 1
 function open_transfer_prompt() {
+}
+
+// Page 11, Figure 2, (1)
+// Relevant API: DeviceStorage
+function remaining_device_storage() {
+}
+
+// Page 11, Figure 2, (2)
+// Relevant API: DeviceStorage
+function application_size() {
+}
+
+// Page 11, Figure 2, (3)
+// Page 14, Figure 2
+// XXX: Might have to look in Bluetooth and DeviceStorage
+//		Also show the amount that's been downloaded so far
+function transfer_complete_percentage() {
 }
